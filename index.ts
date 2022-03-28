d91c74514babffdf2a9b8d17495228adb6927397
Aleksey <a.peresmekhin@action-media.ru>
2022-03-28 22:32:02 +0500

    Configure ts for project

--- index.ts
+++ index.ts
@@ -1,10 +1,8 @@
-import express from 'express';
+import express, { Request, Response, NextFunction} from 'express';
 import {userRouter} from "./users/users.js";
 
-const port = '8000';
+const port = 8000;
 const app = express();
-
-
 
 app.use((req, res, next) => {
   console.log('Time', Date.now());
@@ -17,7 +15,7 @@ app.get('/hello', (req, res) => {
 
 app.use('/users', userRouter);
 
-app.use((err, req, res, _next) => {
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   console.log(err.message);
   res.status(401).send(err.message);
 })
